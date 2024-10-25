a16a085ecb6c459aa5c444ecde0cb4507b2a6df7
badooga <33851191+badooga@users.noreply.github.com>
2024-10-25 05:02:01 -0400

    setup radial_pdf

--- strongholds/probability.py
+++ strongholds/probability.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-from .generate import default_rng, generate_rings, stronghold_grid
-from .types import Iterable, Generator, NVector
+from .chunk_math import inner_radii, outer_radii, stronghold_count
+from .generate import default_rng, generate_rings
+from .types import Generator, Iterable, NVector, ScalarLike
 
-def probability_heatmap(num_samples: int = 10**6,
+__all__ = ["generation_heatmap", "radial_pdf"]
+
+def generation_heatmap(num_samples: int = 10**6,
                         ring_nums: Iterable[int] | None = None,
                         rng: Generator = default_rng) -> NVector:
 
     """
-    For the supplied ring numbers, generates that ring many times and return the result.
+    For the supplied ring numbers, generates those rings the supplied number of times and return the result.
     """
 
     if ring_nums is None:
@@ -19,3 +22,10 @@ def probability_heatmap(num_samples: int = 10**6,
     ])
 
     return stronghold_samples
+
+def radial_pdf(r: ScalarLike, ring_num: int) -> ScalarLike:
+    """Computes the appproximate radial pdf for a given stronghold ring."""
+
+    a, b = inner_radii[ring_num], outer_radii[ring_num]
+    total = stronghold_count[ring_num]
+    return r * 1/total
