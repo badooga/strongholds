cbed939ea2f9dd9f31af5171289cf2aa72350846
badooga <33851191+badooga@users.noreply.github.com>
2024-10-25 02:39:40 -0400

    add locate.py

---
+++ strongholds/locate.py
@@ -0,0 +1,35 @@
+from . import geometry as gm
+
+from .types import RectPoint, RectCoordinates, ScalarLike
+
+__all__ = ["closest_stronghold", "strongholds_in_cone"]
+
+def closest_stronghold(p: RectPoint, strongholds: RectCoordinates) -> RectPoint:
+    """Finds the closest stronghold to a point."""
+
+    distances = gm.distance(p, strongholds)
+    return strongholds[distances.argmin(axis=-1, keepdims=True)].squeeze()
+
+def strongholds_in_cone(p: RectPoint, grid: RectCoordinates,
+                        theta: ScalarLike, theta_err: ScalarLike = 0.05,
+                        error_is_relative: bool = True) -> RectCoordinates:
+    """
+    Finds the possible stronghold locations from an Eye of Ender throw.
+    
+    Keyword arguments:
+        `p`: the location of the player
+        `grid`: the possible stronghold grid points to consider (see `stronghold_grid`)
+        `theta`: the angle of the Eye of Ender throw
+        `theta_err`: the error of the throw
+        `error_is_relative`: whether `theta_err` is absolute or relative
+    """
+
+    r, phi = gm.to_polar(grid - p).T
+    if error_is_relative:
+        theta_b, theta_a = theta * (1 + gm.pm * theta_err)
+    else:
+        theta_b, theta_a = theta + gm.pm * theta_err
+
+    targets = (r > 0) & gm.in_interval(phi, theta_a, theta_b)
+
+    return grid[targets]
