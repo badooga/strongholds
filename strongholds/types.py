e856b20848397f24b56981183bafd89a2bc84edd
badooga <33851191+badooga@users.noreply.github.com>
2024-10-25 02:37:46 -0400

    add types.py

---
+++ strongholds/types.py
@@ -0,0 +1,17 @@
+import numpy as np
+import nptyping as npt
+
+__all__ = ["Generator", "Scalar", "NVector", "ScalarLike",
+           "RectPoint", "PolarPoint", "RectCoordinates", "PolarCoordinates"]
+
+Generator = np.random.Generator
+
+Scalar = float | npt.Float
+NVector = npt.NDArray[npt.Shape["* n"], npt.Float]
+ScalarLike = Scalar | NVector
+
+RectPoint = npt.NDArray[npt.Shape["[x, z]"], npt.Float]
+PolarPoint = npt.NDArray[npt.Shape["[r, phi]"], npt.Float]
+
+RectCoordinates = RectPoint | npt.NDArray[npt.Shape["*, [x, z]"], npt.Float]
+PolarCoordinates = PolarPoint | npt.NDArray[npt.Shape["*, [r, phi]"], npt.Float]
