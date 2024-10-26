63e50106cbbaf354e28b1881467f43630b1de3f9
badooga <33851191+badooga@users.noreply.github.com>
2024-10-26 05:25:36 -0400

    add to_radians

--- strongholds/chunk_math.py
+++ strongholds/chunk_math.py
@@ -1,31 +1,37 @@
 import numpy as np
 
-from .geometry import distance, in_interval
-from .types import RectCoordinates
+from .math import radius, in_interval
+from .types import Coordinates, ScalarLike
 
 stronghold_count = (3, 6, 10, 15, 21, 28, 36, 9)
 inner_radii = (1280, 4352, 7424, 10496, 13568, 16640, 19712, 22784)
 outer_radii = (2816, 5888, 8960, 12032, 15104, 18176, 21248, 24320)
 ring_radii = sum(zip(inner_radii, outer_radii), ())
 
-def snap_chunk(p: RectCoordinates) -> RectCoordinates:
+def to_radians(y_rot: ScalarLike):
+    """Converts Minecraft's y-rotation value to a polar angle in radians."""
+
+    z = np.exp(1j*y_rot * np.pi/180)
+    w = np.conj(1j*z)
+    return np.angle(w)
+
+def snap_chunk(p: Coordinates) -> Coordinates:
     """Snaps coordinates to the northwest corner of the nearest chunk."""
 
     return 16*np.floor(p/16)
 
-def in_ring(p: RectCoordinates, ring_num: int) -> bool:
+def in_ring(p: Coordinates, ring_num: int) -> bool:
     """Checks whether coordinates are in the n-th stronghold ring."""
 
     a, b = inner_radii[ring_num], outer_radii[ring_num]
-    radii = distance(p, 0)
-    return in_interval(radii, a, b)
+    return in_interval(radius(p), a, b)
 
-def closest_ring(p: RectCoordinates) -> int | list[int]:
+def closest_ring(p: Coordinates) -> int | list[int]:
     """Finds the stronghold ring that is closest to the given coordinates."""
 
     in_rings = [n for n in range(8) if in_ring(p, n)]
     if in_rings:
         return in_rings[0]
 
-    radii = distance(p, 0)
+    radii = radius(p)
     return np.floor(np.array([np.abs(radii - r) for r in ring_radii]).argmin()/2)
