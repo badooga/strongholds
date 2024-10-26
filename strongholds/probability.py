8e988965eee13bfa4301fe2e176005ff9bd3f564
badooga <33851191+badooga@users.noreply.github.com>
2024-10-26 05:26:51 -0400

    add TODO label

--- strongholds/probability.py
+++ strongholds/probability.py
@@ -2,13 +2,13 @@ import numpy as np
 
 from .chunk_math import inner_radii, outer_radii, stronghold_count
 from .generate import default_rng, generate_rings
-from .types import Generator, Iterable, NVector, ScalarLike
+from .types import Generator, Iterable, NSequence, ScalarLike
 
 __all__ = ["generation_heatmap", "radial_pdf"]
 
 def generation_heatmap(num_samples: int = 10**6,
                         ring_nums: Iterable[int] | None = None,
-                        rng: Generator = default_rng) -> NVector:
+                        rng: Generator = default_rng) -> NSequence:
 
     """
     For the supplied ring numbers, generates those rings the supplied number of times and return the result.
@@ -28,4 +28,4 @@ def radial_pdf(r: ScalarLike, ring_num: int) -> ScalarLike:
 
     a, b = inner_radii[ring_num], outer_radii[ring_num]
     total = stronghold_count[ring_num]
-    return r * 1/total
+    return r * 1/total # TODO
