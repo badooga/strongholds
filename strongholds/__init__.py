4c46607da585a80bb49882474298029a953eaa6d
badooga <33851191+badooga@users.noreply.github.com>
2024-10-26 02:01:21 -0400

    generation_heatmap

--- strongholds/__init__.py
+++ strongholds/__init__.py
@@ -1,4 +1,4 @@
 from .generate import generation_grid, generate_all
 from .geometry import rectpoint
 from .locate import closest_stronghold, points_in_cone
-from .probability import probability_heatmap
+from .probability import generation_heatmap
