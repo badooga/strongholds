d8ca817c9b407933b6a40dc3e7c3e03f6efaa69b
badooga <33851191+badooga@users.noreply.github.com>
2024-10-26 05:26:04 -0400

    simplify functions, update biome_snap to complex

--- strongholds/generate.py
+++ strongholds/generate.py
@@ -1,46 +1,53 @@
 import numpy as np
 
 from . import chunk_math as cm
-from . import geometry as gm
+from . import math as gm
 
-from .types import Iterable, Generator, RectCoordinates
+from .types import Iterable, Generator, Coordinates
 
 default_rng = np.random.default_rng(0)
 
-def generation_grid(ring_nums: Iterable | None = None) -> RectCoordinates:
+def generation_grid(ring_nums: Iterable | None = None) -> Coordinates:
     """Returns a grid of possible stronghold points in the supplied rings."""
 
     if ring_nums is None:
         ring_nums = range(8)
 
-    j = np.arange(-24496/16, 24496/16)
-    X = Z = 16*j + 8
+    i = np.arange(-24496/16, 24496/16)
+    x = z = 16*i + 8
+    X, Z = np.meshgrid(x, z)
+    grid = gm.rectpoint(X, Z).flatten()
 
-    grid = np.array(np.meshgrid(X, Z)).T.reshape(-1, 2)
     rings = np.any([cm.in_ring(grid, n) for n in ring_nums], axis=0)
     return grid[rings]
 
 def generate_ring(ring_num: int, snap: bool = True,
-                  rng: Generator = default_rng) -> RectCoordinates:
+                  rng: Generator = default_rng) -> Coordinates:
     """Generates stronghold coordinates in a given ring."""
 
     n = cm.stronghold_count[ring_num]
     a, b = cm.inner_radii[ring_num], cm.outer_radii[ring_num]
 
-    phi = rng.uniform(0, 2*np.pi) + gm.unity_angles(n)
     r = rng.uniform(a, b, n)
-    P = gm.to_rect(gm.polarpoint(r, phi))
+    phi = rng.uniform(0, 2*np.pi) + gm.unity_angles(n)
+    P = gm.polarpoint(r, phi)
+
     if snap:
-        P = cm.snap_chunk(P) + 16 * rng.integers(-7, 8, (n, 2)) + 8
+        # first, snaps biome to (0, 0) of chunk
+        P = cm.snap_chunk(P)
+        # next, snaps to uniformly chosen biome center up to 7 chunks away
+        biome_snap = 16 * rng.integers(-7, 8, (2, n)) + 8
+        P.real += biome_snap[0]
+        P.imag += biome_snap[1]
     return P
 
 def generate_rings(ring_nums: Iterable, snap: bool = True,
-                   rng: Generator = default_rng) -> RectCoordinates:
+                   rng: Generator = default_rng) -> Coordinates:
     """Generates stronghold coordinates in given rings."""
 
     return np.concatenate([generate_ring(n, snap, rng) for n in ring_nums])
 
-def generate_all(snap: bool = True, rng: Generator = default_rng) -> RectCoordinates:
+def generate_all(snap: bool = True, rng: Generator = default_rng) -> Coordinates:
     """Generates all 128 random strongholds a world can have."""
 
     return generate_rings(range(8), snap, rng)
