6fdfd7511e328edff91051d291f6faf3660298e8
badooga <33851191+badooga@users.noreply.github.com>
2024-10-28 05:23:05 -0400

    split strongholds into multiple notebooks

--- strongholds/__init__.py
+++ strongholds/__init__.py
@@ -1,3 +1,4 @@
+from .chunk_math import to_radians, to_yrot
 from .generate import generation_grid, generate_all
 from .math import angle, radius, rectpoint, polarpoint
 from .locate import closest_stronghold, points_in_cone
