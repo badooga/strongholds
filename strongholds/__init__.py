f7024c21c9c690338834996050cc12da4979a3fb
badooga <33851191+badooga@users.noreply.github.com>
2024-10-28 04:17:31 -0400

    add angle and radius imports

--- strongholds/__init__.py
+++ strongholds/__init__.py
@@ -1,4 +1,4 @@
 from .generate import generation_grid, generate_all
-from .math import rectpoint, polarpoint
+from .math import angle, radius, rectpoint, polarpoint
 from .locate import closest_stronghold, points_in_cone
 from .probability import generation_heatmap
