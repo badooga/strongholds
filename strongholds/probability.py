8b74ae0397c4127e93ba295b5c4c3330d3ef258b
badooga <33851191+badooga@users.noreply.github.com>
2024-10-29 00:58:22 -0400

    add snap parameter to generation_heatmap

--- strongholds/probability.py
+++ strongholds/probability.py
@@ -10,17 +10,19 @@ __all__ = ["generation_heatmap", "radial_pdf"]
 def generation_heatmap(num_samples: int = 10**6,
                         ring_nums: Iterable[int] | None = None,
                         rng: Generator = default_rng,
+                        snap: bool = True,
                         concatenate: bool = True) -> NSequence | Iterable[Coordinates]:
 
     """
-    For the supplied ring numbers, generates those rings the supplied number of times and return the result.
+    For the supplied ring numbers, generates those rings
+    the supplied number of times and return the result.
     """
 
     if ring_nums is None:
         ring_nums = range(8)
 
     stronghold_samples = np.array([
-        generate_rings(ring_nums, True, rng) for _ in range(num_samples)
+        generate_rings(ring_nums, snap, rng) for _ in range(num_samples)
     ])
 
     if concatenate:
@@ -35,7 +37,6 @@ def closest_stronghold_heatmap(p: Point, stronghold_sets: Iterable[Coordinates])
     """
 
     return np.array([closest_stronghold(p, strongholds) for strongholds in stronghold_sets])
-
 
 def radial_pdf(r: ScalarLike, ring_num: int) -> ScalarLike:
     """Computes the appproximate radial pdf for a given stronghold ring."""
