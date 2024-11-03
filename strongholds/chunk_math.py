bd92a36637d3304951bc9f38112b84a8800f9715
badooga <33851191+badooga@users.noreply.github.com>
2024-11-03 00:20:38 -0400

    add ring_centers

--- strongholds/chunk_math.py
+++ strongholds/chunk_math.py
@@ -6,6 +6,8 @@ from .types import Coordinates, Iterable, ScalarLike
 stronghold_count = np.array([3, 6, 10, 15, 21, 28, 36, 9])
 inner_radii = np.array([1280, 4352, 7424, 10496, 13568, 16640, 19712, 22784])
 outer_radii = np.array([2816, 5888, 8960, 12032, 15104, 18176, 21248, 24320])
+
+ring_centers = (inner_radii + outer_radii)/2
 ring_radii = np.dstack((inner_radii, outer_radii)).flatten()
 
 def to_radians(y_rot: ScalarLike) -> ScalarLike:
